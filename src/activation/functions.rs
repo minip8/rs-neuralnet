b46e1e22be725f961eb79f89a580e9ab061691bc
Matthew Yang <mattyang1340@gmail.com>
2026-03-25 21:54:20 +1100

    refactor(functions): F for Float

--- src/activation/functions.rs
+++ src/activation/functions.rs
@@ -1,26 +1,26 @@
 use num_traits::Float;
 
-pub fn relu<T: Float>(x: T) -> T {
-    x.max(T::zero())
+pub fn relu<F: Float>(x: F) -> F {
+    x.max(F::zero())
 }
 
-pub fn relu_backward<T: Float>(x: T) -> T {
-    if x > T::zero() { T::one() } else { T::zero() }
+pub fn relu_backward<F: Float>(x: F) -> F {
+    if x > F::zero() { F::one() } else { F::zero() }
 }
 
-pub fn sigmoid<T: Float>(x: T) -> T {
-    T::one() / (T::one() + (-x).exp())
+pub fn sigmoid<F: Float>(x: F) -> F {
+    F::one() / (F::one() + (-x).exp())
 }
 
-pub fn sigmoid_backward<T: Float>(x: T) -> T {
+pub fn sigmoid_backward<F: Float>(x: F) -> F {
     let s = sigmoid(x);
-    s * (T::one() - s)
+    s * (F::one() - s)
 }
 
-pub fn tanh<T: Float>(x: T) -> T {
+pub fn tanh<F: Float>(x: F) -> F {
     x.tanh()
 }
 
-pub fn tanh_backward<T: Float>(x: T) -> T {
-    T::one() - x.tanh().powi(2)
+pub fn tanh_backward<F: Float>(x: F) -> F {
+    F::one() - x.tanh().powi(2)
 }
