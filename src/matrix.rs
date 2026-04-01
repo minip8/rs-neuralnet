2b6bdeee1e976fb7188e9c39bddf522dba33a2b8
Matthew Yang <mattyang1340@gmail.com>
2026-04-01 12:22:20 +1100

    fix(matrix): fix dot_rows_with_row

--- src/matrix.rs
+++ src/matrix.rs
@@ -349,7 +349,7 @@ impl<F: Float> Matrix<F> {
         for i in 0..self.rows {
             data[i] = {
                 let start = i * self.cols;
-                let end = i * (self.cols + 1);
+                let end = start + self.cols;
                 self.data[start..end]
                     .iter()
                     .zip(other.data.iter())
