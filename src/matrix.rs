4aefebcbdf6e0d03fb9621646abdea5c8ce3dbeb
Matthew Yang <mattyang1340@gmail.com>
2026-04-01 12:20:37 +1100

    fix(matrix): fix add_row_to_all_rows

--- src/matrix.rs
+++ src/matrix.rs
@@ -237,7 +237,7 @@ impl<F: Float> Matrix<F> {
         let mut res = Self::zeros(self.rows, self.cols);
         for i in 0..self.rows {
             for j in 0..self.cols {
-                res.set_(i, j, res.get(i, j) + other.get(0, j));
+                res.set_(i, j, self.get(i, j) + other.get(0, j));
             }
         }
         res
